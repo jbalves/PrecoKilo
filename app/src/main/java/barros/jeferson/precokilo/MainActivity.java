6f659511a84d209711c0685b3297412917e01cb4
Jeferson Barros <im.jbalves@gmail.com>
2017-08-21 17:18:20 -0400

    Initial main class

--- app/src/main/java/barros/jeferson/precokilo/MainActivity.java
+++ app/src/main/java/barros/jeferson/precokilo/MainActivity.java
@@ -2,12 +2,30 @@ package barros.jeferson.precokilo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Calcular(View view) {
+        EditText precoKilo = (EditText) findViewById(R.id.edPrecoKilo);
+        EditText edPeso = (EditText) findViewById(R.id.edPeso);
+        TextView txtExibirValor = (TextView) findViewById(R.id.txtExibirValor);
+
+        float valorPagar = 0;
+        float valorKilo = Float.valueOf(precoKilo.getText().toString());
+        float valorPeso = Float.valueOf(edPeso.getText().toString());
+        valorPagar = (valorKilo * valorPeso)/1000;
+        Log.d("Debug", valorPagar+"");
     }
 }
